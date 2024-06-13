6ef0e09bf4e1a0ba903414b489945ed05b09ba20
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-06-13 18:28:08 +0200

    Add weather API module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import 'normalize.css';
 import './index-style.css';
+import getLocationData from './modules/weather';
+import updateWeatherInfo from './modules/domController';
+
+const locationData = getLocationData('london');
+locationData.then((res) => console.log(res));
+locationData.then((res) => updateWeatherInfo(res, 'C'));
---
+++ src/modules/weather.js
@@ -0,0 +1,79 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-plusplus */
+import { fetchWeatherApi } from 'openmeteo';
+
+const range = require('lodash.range');
+
+const getCoords = async (location) => {
+    try {
+        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=1&language=en&format=json`, { mode: 'cors' });
+
+        const data = await response.json();
+        return data.results[0];
+    } catch (err) {
+        return err;
+    }
+};
+
+export default async function getLocationData(location) {
+    const locationCoords = await getCoords(location);
+
+    const params = {
+        latitude: locationCoords.latitude,
+        longitude: locationCoords.longitude,
+        current: ['temperature_2m', 'relative_humidity_2m', 'apparent_temperature', 'precipitation', 'weather_code'],
+        hourly: ['temperature_2m', 'precipitation', 'weather_code', 'uv_index'],
+        daily: ['weather_code', 'temperature_2m_max', 'temperature_2m_min', 'sunset', 'uv_index_max'],
+        forecast_days: 14,
+        forecast_hours: 6,
+    };
+
+    const url = 'https://api.open-meteo.com/v1/forecast';
+    const responses = await fetchWeatherApi(url, params);
+
+    // Process first location
+    const response = responses[0];
+
+    // Attributes for timezone and location
+    const utcOffsetSeconds = response.utcOffsetSeconds();
+
+    const current = response.current();
+    const hourly = response.hourly();
+    const daily = response.daily();
+
+    // Note: The order of weather variables in the URL query and the indices below need to match!
+    const weatherData = {
+        location: {
+            name: locationCoords.name,
+        },
+        current: {
+            time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+            temperature2m: current.variables(0).value(),
+            relativeHumidity2m: current.variables(1).value(),
+            apparentTemperature: current.variables(2).value(),
+            precipitation: current.variables(3).value(),
+            weatherCode: current.variables(4).value(),
+        },
+        hourly: {
+            time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
+                (t) => new Date((t + utcOffsetSeconds) * 1000),
+            ),
+            temperature2m: hourly.variables(0).valuesArray(),
+            precipitation: hourly.variables(1).valuesArray(),
+            weatherCode: hourly.variables(2).valuesArray(),
+            uvIndex: hourly.variables(3).valuesArray(),
+        },
+        daily: {
+            time: range(Number(daily.time()), Number(daily.timeEnd()), daily.interval()).map(
+                (t) => new Date((t + utcOffsetSeconds) * 1000),
+            ),
+            weatherCode: daily.variables(0).valuesArray(),
+            temperature2mMax: daily.variables(1).valuesArray(),
+            temperature2mMin: daily.variables(2).valuesArray(),
+            sunset: daily.variables(3).valuesArray(),
+            uvIndexMax: daily.variables(4).valuesArray(),
+        },
+
+    };
+    return weatherData;
+}
